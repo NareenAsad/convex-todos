c0b507fa84c8c91bcea4ab4a151753a39d6e9179
Nareen Asad <bennynanny80@gmail.com>
2024-09-17 15:02:01 +0500

    changes

--- src/app/convex-client-provider.tsx
+++ src/app/convex-client-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import { ConvexClient } from "convex/browser";
 
 const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
